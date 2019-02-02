ab6f3e2bd1c4506b11a2b4b480e2f1225090f781
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-02-02 12:27:29 +0100

    nacrtali robota

--- sketch.js
+++ sketch.js
@@ -13,7 +13,15 @@ function setup() {
  ellipse(width/2-50+25, height/2-50+30, 20, 20);
  ellipse(width/2-50+70, height/2-50+30, 30, 30);
  line(width/2-30, height/2+30, width/2+30, height/2+30);
+
+   stroke(0); //crna linija
+  line(width/2-30, height/2+30, width/2+30, height/2+30)
+
+  noStroke(); //nema okvir
+  rect(width/2-30, height/2+18, 10, 10); //zub 1
+  rect(width/2+20, height/2+18, 10, 10); // zub 2
  
+ stroke(0);
  translate(width/2-60,height/2-60);
 fill(255,100,30);
  triangle(0, 0, 0, 60, 60, 0);
