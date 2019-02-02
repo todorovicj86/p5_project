1c2d2f9847f3aac658afc055eff8a46023d0c13c
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-02-02 12:14:51 +0100

    Nacrtati oci

--- sketch.js
+++ sketch.js
@@ -7,6 +7,12 @@ function setup() {
   fill(0,170,0);
 
   rect(width/2-50, height/2-50, 100, 100); //rect(x,y, width, height)
+ 
+ fill(255); //kaze koje boje je sledeci objekat, pa ide pre crtanja objekta
+
+ ellipse(width/2-50+25, height/2-50+30, 20, 20);
+ ellipse(width/2-50+70, height/2-50+30, 30, 30);
+
 }
 
  function draw() {
