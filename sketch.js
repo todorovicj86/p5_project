fa329ec8d27aadd00de35ae580bf0cb1c5fabc70
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-02-02 12:08:51 +0100

    nacrtaj kvadrat

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  background(0,100,0);
+
+  strokeWeight(4); //debljina linije
+
+  fill(0,170,0);
+
+  rect(width/2-50, height/2-50, 100, 100); //rect(x,y, width, height)
 }
 
  function draw() {
-  background(0,100,0);
+  
 }
