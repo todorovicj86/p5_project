9da8b0852d0a50f918229ae5749beb0828b756ea
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-02-02 12:48:15 +0100

    Polozaj usta ne radi

--- sketch.js
+++ sketch.js
@@ -1,38 +1,59 @@
-var positionX=0;
-var positionY=0;
-
+var positionX = 0;
+var positionY = 0;
 
 function setup() {
   createCanvas(400, 300);
-
 }
 
 function draw() {
-
   background(200);
 
-  positionX=mouseX;
-  positionY=mouseY;
+  positionX = mouseX;
+  positionY = mouseY;
+
+  nacrtajTelo(positionX, positionY);
+  nacrtajOci();
+  nacrtajKapu();
 
 
+  if (mouseIsPressed) {
+    nacrtajUsta(true);
+  } else {
+    nacrtajUsta(false);
+  }
+}
+
+function nacrtajTelo(x, y) {
   strokeWeight(4);
-
   fill(0,170,0);
-  rect(positionX-50, positionY-50, 100, 100);
+  rect(x - 50, y - 50, 100, 100);
+}
 
-  fill (255);
-  ellipse(positionX-50+25, positionY-50+30, 20, 20)
-  ellipse(positionX-50+70, positionY-50+30, 30, 30)
-
+function nacrtajKapu() {
   stroke(0);
-  line(positionX-30, positionY+30, positionX+30, positionY+30)
-
-  noStroke();
-  rect(positionX-30, positionY+18, 10, 10);
-  rect(positionX+20, positionY+18, 10, 10);
-
-  stroke(0);
-  translate(positionX-60,positionY-60);
+  translate(positionX - 60, positionY - 60);
   fill(255,100,30);
   triangle(0, 0, 0, 60, 60, 0);
+}
+
+function nacrtajOci() {
+  fill(255);
+  ellipse(positionX - 50 + 25, positionY - 50 + 30, 20, 20)
+  ellipse(positionX - 50 + 70, positionY - 50 + 30, 30, 30)
+}
+
+function nacrtajUsta(zatvorena) {
+  if (zatvorena) {
+    stroke(0);
+    line(positionX -30 , positionY + 30, positionX + 30, positionY + 30)
+  } else {
+    noStroke();
+    fill(255, 0, 0);
+    rect(positionX - 30, positionY + 13, 60, 20);
+  }
+
+  fill(255);
+  noStroke();
+  rect(positionX - 30, positionY + 18, 10, 10);
+  rect(positionX + 20, positionY + 18, 10, 10);
 }
